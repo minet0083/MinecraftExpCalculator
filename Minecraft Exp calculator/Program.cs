9cf457f3d8c3d190f8a1e01429b66c72700739e8
minet0083 <minet0083@gmail.com>
2014-11-03 01:18:51 +0900

    Program.cs更新

--- Minecraft Exp calculator/Program.cs
+++ Minecraft Exp calculator/Program.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
-namespace Minecraft_Exp_calculator
+namespace MinecraftExpCalculator
 {
     static class Program
     {
@@ -16,7 +16,7 @@ namespace Minecraft_Exp_calculator
         {
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new AppWindow());
         }
     }
 }
